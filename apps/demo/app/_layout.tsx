047b513eef1aa05c38758640e36d73be418cb03d
rygrams <bamorygramboute@gmail.com>
2026-01-20 01:02:44 +0000

    chore: Remove demo app component examples and associated `@xaui/colors` and `@xaui/core` dependencies.

--- apps/demo/app/_layout.tsx
+++ apps/demo/app/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import 'react-native-reanimated'
 import { useColorScheme } from '@/hooks/use-color-scheme'
-import { XUIProvider } from '@xaui/core'
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,17 +12,12 @@ export default function RootLayout() {
   const colorScheme = useColorScheme()
 
   return (
-    <XUIProvider>
-      <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="modal"
-            options={{ presentation: 'modal', title: 'Modal' }}
-          />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </XUIProvider>
+    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   )
 }
