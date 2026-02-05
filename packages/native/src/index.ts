2331af2d9d142439536eff2cc70f7075ee64c12a
Bamory GRAMBOUTE <ladjigramboute@arolitec.com>
2026-02-05 15:46:01 +0000

    Remove  related type exports from .

--- packages/native/src/index.ts
+++ packages/native/src/index.ts
@@ -1,11 +1,4 @@
 export { ActivityIndicator } from './components/indicator'
 export { Divider } from './components/divider'
 export { Autocomplete, AutocompleteItem } from './components/autocomplete'
-export type {
-  AutocompleteProps,
-  AutocompleteItemProps,
-  AutocompleteVariant,
-  AutocompleteLabelPlacement,
-  AutocompleteMenuTrigger,
-} from './components/autocomplete'
 export { Typography } from './components/typography'
